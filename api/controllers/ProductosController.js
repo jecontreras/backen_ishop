d25996395ead98441ce590257ce5d0051541cf47
luisalbertoj <luisalbertoj.tober@gmail.com>
2020-03-17 12:08:28 -0500

    fix: se trabajo en el cliente para usuarios

--- api/controllers/ProductosController.js
+++ api/controllers/ProductosController.js
@@ -22,13 +22,42 @@ Procedures.querys = async (req, res) => {
 }
 
 Procedures.upload = async (req, res) => {
-    let allParams = req.allParams();     
+    let params = req.allParams();     
     let response = Object();
-    response.img1 = await UploadService(req.file('fileKey0'));
-    response.img2 = await UploadService(req.file('fileKey1'));
-    response.img3 = await UploadService(req.file('fileKey2'));
-    response.img4 = await UploadService(req.file('fileKey3'));
-    response.img5 = await UploadService(req.file('fileKey4'));
+    if(req.file('fileKey0')) response.img1 = await UploadService(req.file('fileKey0'));
+    if(req.file('fileKey1')) response.img2 = await UploadService(req.file('fileKey1'));
+    if(req.file('fileKey2')) response.img3 = await UploadService(req.file('fileKey2'));
+    if(req.file('fileKey3')) response.img4 = await UploadService(req.file('fileKey3'));
+    if(req.file('fileKey4')) response.img5 = await UploadService(req.file('fileKey4'));
+
+    if(response.img1) {
+        Productos.create(params.producto).exec(
+            (error, responsePro) => {
+                if(error) return res.ok(error);
+                Multimedia.create({
+                    recurso: response.img1,
+                    estado: 0
+                }).exec(
+                    (error, responseMulti) => {
+                        if(error) return res.ok(error);
+                        ProductosMultimedia.create({
+                            idProducto: responsePro.id,
+                            idMultimedia: responseMulti.id,
+                            estado: 0
+                        }).exec(
+                            (error, responseJoin) => {
+                                if(error) return res.ok(error);
+                                return res.ok({responseJoin});
+                            }
+                        )
+                    }
+                )
+            }
+        );
+    } else {
+        res.ok({msg: 'la peticion no trae imagen de producto'});
+    }
+    
     return res.ok( { status: 200, ...response } );
 },
 
