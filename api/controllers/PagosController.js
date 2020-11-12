57364e5e48dfd90ac37e15c7cd300b812e3e5864
jecontreras <joseeduar147@gmail.com>
2020-11-12 10:58:44 -0500

    cambios new

--- api/controllers/PagosController.js
+++ api/controllers/PagosController.js
@@ -14,4 +14,16 @@ Procedures.querys = async(req, res)=>{
     return res.ok( { status: 200, ...resultado } );
 }
 
+Procedures.create = async( req, res )=>{
+  let params = req.allParams();
+  let resultado = Object();
+  resultado = await ResumenCuenta.validandoEntrada( {
+    valorEntrante: params.monto,
+    tipoEntrada: 1,
+    idPersona: params.idPersona
+  } );
+  resultado = await Pagos.create( params ).fetch();
+  return res.status( 200 ).send( resultado );
+}
+
 module.exports = Procedures;
